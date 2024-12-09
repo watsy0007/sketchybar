cabebf80eae18732980399b7f8b1be4607f85604
watsy0007 <watsy0007@gmail.com>
2024-12-09 08:24:13 +0800

    use aerospace

---
+++ plugins/aerospace/item.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+for sid in $(aerospace list-workspaces --all); do
+  sketchybar --add item space.$sid left \
+    --subscribe space.$sid aerospace_workspace_change \
+    --set space.$sid \
+    background.color=0x44ffffff \
+    background.corner_radius=20 \
+    background.height=20 \
+    background.border_color=0x00FFFF \
+    padding_left=7 \
+    padding_right=7 \
+    background.drawing=off \
+    label="$sid" \
+    click_script="aerospace workspace $sid" \
+    script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+done
